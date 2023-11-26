5f6547f61f1b9bfe8ead69a8df5e0e036ccf2fae
tongzheming2020 <tongzheming@fangxingren.com>
2023-11-26 21:02:34 +0800

    update test

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ from face_detection.mtcnn_detector import count_faces
 
 class TestFaceDetection(unittest.TestCase):
     def test_detect_faces(self):
-        # 测试用例1：没有人脸的图片
+        # 测试用例1：有4个人脸的图片
         image_path = 'images/4_faces_2.jpg' 
         num_faces = count_faces(image_path)
         # 从文件名中读取真实的人脸数量
@@ -14,7 +14,7 @@ class TestFaceDetection(unittest.TestCase):
         true_num_faces = int(filename.split('_')[0])
         self.assertEqual(num_faces, true_num_faces)
 
-        # 测试用例2：有一个人脸的图片
+        # 测试用例2：有16人脸的图片
         image_path = 'images/16_faces_1.jpeg'  
         num_faces = count_faces(image_path)
         # 从文件名中读取真实的人脸数量
